2ba6aa3bc1ebc087acb36828ff1be808c6291c4b
Nathan <nathanlserrano@gmail.com>
2018-11-29 13:55:54 -0800

    added particle effects to only appear for results page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,7 +3,7 @@ particlesJS("particles-js", {
       number: { value: 80, density: { enable: true, value_area: 800 } },
       color: { value: "#000000" },
       shape: {
-        type: "circle",
+        type: "star",
         stroke: { width: 0, color: "#000000" },
         polygon: { nb_sides: 5 },
         image: { src: "img/github.svg", width: 100, height: 100 }
@@ -53,3 +53,5 @@ particlesJS("particles-js", {
     },
     retina_detect: true
   });
+
+  console.log("particles.js is here...")
